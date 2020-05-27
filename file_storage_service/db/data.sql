f1a4ba631e5452e1627ba9c526694a9bbcef0934
GinesGarcia <gines.garcia2@gmail.com>
2020-05-27 15:37:26 +0200

    Common naming for site facilities

--- file_storage_service/db/data.sql
+++ file_storage_service/db/data.sql
@@ -1,7 +1,7 @@
 -- Insert initial data here
-insert into sitedata (_id, sitename) values ('5781d2c6-cfd3-4243-bf0b-c1d51bb90549', 'france_nice');
-insert into sitedata (_id, sitename) values ('8853f685-18e7-4da3-83dc-20a80a42caff', 'france_paris');
-insert into sitedata (_id, sitename) values ('33b30c0f-c65b-47d4-bc3d-95d0dc862f40', 'france_rennes');
-insert into sitedata (_id, sitename) values ('13a4fea9-9664-4fe3-b115-d9ad5607c5aa', 'greece_athens');
-insert into sitedata (_id, sitename) values ('908868f2-9a7f-440a-8fde-5a610b67fd6b', 'italy_turin');
-insert into sitedata (_id, sitename) values ('37fbe0b2-8907-4996-927f-6f6ee6ed45be', 'spain_5tonic');
+insert into sitedata (_id, sitename) values ('5781d2c6-cfd3-4243-bf0b-c1d51bb90549', 'FRANCE_NICE');
+insert into sitedata (_id, sitename) values ('8853f685-18e7-4da3-83dc-20a80a42caff', 'FRANCE_PARIS');
+insert into sitedata (_id, sitename) values ('33b30c0f-c65b-47d4-bc3d-95d0dc862f40', 'FRANCE_RENNES');
+insert into sitedata (_id, sitename) values ('13a4fea9-9664-4fe3-b115-d9ad5607c5aa', 'GREECE_ATHENS');
+insert into sitedata (_id, sitename) values ('908868f2-9a7f-440a-8fde-5a610b67fd6b', 'ITALY_TURIN');
+insert into sitedata (_id, sitename) values ('37fbe0b2-8907-4996-927f-6f6ee6ed45be', 'SPAIN_5TONIC');
